8024c66224fd0acab0ce46da0b6a3d71957c6d73
dacorie <dacor101@gmail.com>
2021-08-12 09:17:33 -0500

    update

--- app/src/main/java/com/example/marketalerts/api/retrofitmodel/InstrumentQuote.kt
+++ app/src/main/java/com/example/marketalerts/api/retrofitmodel/InstrumentQuote.kt
@@ -1,3 +1,38 @@
 package com.example.marketalerts.api.retrofitmodel
 
-//ggg
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class InstrumentQuote {
+    @SerializedName("message")
+    @Expose
+    var indices: String? = null
+
+    @SerializedName("success")
+    @Expose
+    var get_50day_moving_avg: Boolean? = null
+
+    @SerializedName("success")
+    @Expose
+    var get_200day_moving_avg: Boolean? = null
+
+    @SerializedName("success")
+    @Expose
+    var get_current_price: Boolean? = null
+}
+
+
+/*
+Response body
+Download
+{
+    "indices": "AAPL",
+    "data": {
+    "get_50day_moving_avg": 143.55705,
+    "get_200day_moving_avg": 132.13638,
+    "get_stock_summary_url": "https://finance.yahoo.com/quote/AAPL",
+    "get_current_price": 145.86
+},
+    "createdOn": "2021-08-12T06:51:58.563690",
+    "errors": []
+}*/
